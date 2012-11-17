670141704bfeef67d5e27453015394f6d293ae41
Colin Copeland <copelco@caktusgroup.com>
2012-11-17 15:51:13 -0500

    add south dependency

--- broadcast/migrations/0001_initial.py
+++ broadcast/migrations/0001_initial.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("groups", "0001_initial"),
+    )
+
     def forwards(self, orm):
         # Adding model 'DateAttribute'
         db.create_table('broadcast_dateattribute', (
