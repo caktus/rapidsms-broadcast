dea0edfcbabdad1eff380f079c7b5ea33c65241d
Rebecca Lovewell <rebecca@caktusgroup.com>
2012-11-30 10:11:05 -0500

    Use 0-6 (Monday-Sunday) for weekday attributes

--- broadcast/models.py
+++ broadcast/models.py
@@ -22,13 +22,13 @@ class DateAttribute(models.Model):
         ('month', 'Month'),
     )
     WEEKDAY_CHOICES = (
-        (0, 'Sunday'),
-        (1, 'Monday'),
-        (2, 'Tuesday'),
-        (3, 'Wednesday'),
-        (4, 'Thursday'),
-        (5, 'Friday'),
-        (6, 'Saturday'),
+        (0, 'Monday'),
+        (1, 'Tuesday'),
+        (2, 'Wednesday'),
+        (3, 'Thursday'),
+        (4, 'Friday'),
+        (5, 'Saturday'),
+        (6, 'Sunday'),
     )
     MONTH_CHOICES = (
         (1, 'January'),
