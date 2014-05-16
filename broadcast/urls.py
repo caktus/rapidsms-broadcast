ebc52008dfa499abef248b1d3aa508e1a6185995
Tobias McNulty <tobias@caktusgroup.com>
2014-05-16 16:05:21 -0400

    update urls.py for Django 1.6

--- broadcast/urls.py
+++ broadcast/urls.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 
 from broadcast import views
 
